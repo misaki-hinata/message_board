5b33dd0357460fe7b12278af0057d2343aa35195
hinata <hinata@NOTE1016>
2021-07-26 10:58:45 +0900

    Index/NewServletの修正

--- src/main/java/controllers/NewServlet.java
+++ src/main/java/controllers/NewServlet.java
@@ -1,9 +1,10 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
+//import java.sql.Timestamp;
 
-import javax.persistence.EntityManager;
+//import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
-import utils.DBUtil;
+//import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -31,6 +32,16 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
+
+        // おまじないとしてのインスタンスを生成
+        request.setAttribute("message", new Message());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        rd.forward(request, response);
+
+        /*
         EntityManager em = DBUtil.createEntityManager();
         em.getTransaction().begin();
 
@@ -57,6 +68,7 @@ public class NewServlet extends HttpServlet {
         response.getWriter().append(Integer.valueOf(m.getId()).toString());
 
         em.close();
+        */
     }
 
 }
